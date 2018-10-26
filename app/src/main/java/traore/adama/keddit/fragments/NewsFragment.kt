6e6f50f0ecaaf21d88d0805edca02dd6cb62aeaa
Adama <a.traore.pro@gmail.com>
2018-10-26 17:36:34 +0200

    lazy delegate recyclerview

--- app/src/main/java/traore/adama/keddit/fragments/NewsFragment.kt
+++ app/src/main/java/traore/adama/keddit/fragments/NewsFragment.kt
@@ -2,18 +2,31 @@ package traore.adama.keddit.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import traore.adama.keddit.R
 import traore.adama.keddit.tools.inflate
+import kotlinx.android.synthetic.main.news_fragment.*
 
 public class NewsFragment : Fragment(){
 
+    private val newsList: RecyclerView by lazy {
+        rcvNews
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = container?.inflate(R.layout.news_fragment)
 
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        newsList.setHasFixedSize(true)
+        newsList.layoutManager = LinearLayoutManager(context)
     }
 }
